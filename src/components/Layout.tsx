4fb304845a7c52c161469e391c2b15478f419032
pixelg <pixelguerrilla@gmail.com>
2025-01-18 05:09:25 -0600

    Final for the night.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import {ThemeProvider} from "@/components/theme-provider.tsx";
 export default function Layout({children} : {children: React.ReactNode}) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className='bg-white text-black dark:bg-gray-800 dark:text-stone-200 p-4'>
+      <div className='bg-white text-black dark:bg-gray-800 dark:text-white p-4'>
         {children}
       </div>
     </ThemeProvider>
--- src/components/ui/Widget.tsx
+++ src/components/ui/Widget.tsx
@@ -3,7 +3,7 @@ import './Widget.css';
 function Widget(props: { name: string; }) {
   return (
     <div className='w-1/2 h-1/2 bg-amber-400 rounded-xl p-4 m-4'>
-      <h2 className='text-white dark:text-black text-3xl p-1'>{props.name && props.name.toUpperCase()}</h2>
+      <h2 className='text-3xl p-1'>{props.name && props.name.toUpperCase()}</h2>
       <p className='color-me-special p-2'>This is a widget.</p>
     </div>
   );
