58b1be18c2803b0faf067db1e924a4ef1e609247
blee <pixelguerrilla@gmail.com>
2025-02-18 17:23:26 -0700

    Remove unused SVG assets and update dependencies.
    Deleted `react.svg` and `vite.svg` from the project to reduce unnecessary assets. Updated multiple dependencies in `pnpm-lock.yaml` including `@types/react`, `@tanstack/react-query`, and Storybook-related packages for better compatibility and performance.

--- src/app/routes/dashboard.tsx
+++ src/app/routes/dashboard.tsx
@@ -1,20 +1,35 @@
+import logo from "@/assets/metric-minds-logo.svg";
+
 export const DashboardRoute = () => {
   return (
-    <div className="w-full h-screen container mx-auto p-10">
-      <h1 className="text-3xl font-bold mb-8">Welcome to Metric Mind Jams!</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-        suscipit ligula et porttitor lacinia. Praesent blandit augue nec feugiat
-        mattis. Integer efficitur, odio non ultrices dictum, felis turpis
-        aliquam nulla, sed convallis augue ante sit amet urna. Mauris ut mattis
-        felis, nec tristique tellus. Proin dictum libero nec dolor sodales, id
-        hendrerit nibh fermentum. Vestibulum hendrerit nisl at lacus fermentum,
-        ac congue ligula malesuada. Pellentesque gravida viverra nunc vel
-        convallis. Aliquam pulvinar metus a sem rhoncus accumsan. Sed congue,
-        nisi eget venenatis imperdiet, lacus lorem sodales massa, vel pulvinar
-        augue lacus et arcu. Nullam pretium, arcu vitae volutpat rhoncus, ipsum
-        eros tincidunt velit, id feugiat neque lacus vel odio.
-      </p>
-    </div>
+    <>
+      <div className="w-full h-screen container mx-auto p-10">
+        <div className="flex items-center gap-x-4 py-6">
+          <img
+            className="size-12 shrink-0"
+            src={logo}
+            alt="Metric Minds Logo"
+          />
+          <div className="text-xl font-medium text-black dark:text-white">
+            Welcome to Metric Minds Dashboard!
+          </div>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          suscipit ligula et porttitor lacinia. Praesent blandit augue nec
+          feugiat mattis. Integer efficitur, odio non ultrices dictum, felis
+          turpis aliquam nulla, sed convallis augue ante sit amet urna. Mauris
+          ut mattis felis, nec tristique tellus. Proin dictum libero nec dolor
+          sodales, id hendrerit nibh fermentum. Vestibulum hendrerit nisl at
+          lacus fermentum, ac congue ligula malesuada. Pellentesque gravida
+          viverra nunc vel convallis. Aliquam pulvinar metus a sem rhoncus
+          accumsan. Sed congue, nisi eget venenatis imperdiet, lacus lorem
+          sodales massa, vel pulvinar augue lacus et arcu. Nullam pretium, arcu
+          vitae volutpat rhoncus, ipsum eros tincidunt velit, id feugiat neque
+          lacus vel odio.
+        </p>
+      </div>
+    </>
   );
 };
--- src/app/routes/landing.tsx
+++ src/app/routes/landing.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button.tsx";
 import { login } from "@/lib/spotify-auth.ts";
+import logo from "@/assets/metric-minds-logo.svg";
 
 export const LandingRoute = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold mb-8">Metric Minds</h1>
+    <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      <img className="size-12 shrink-0" src={logo} alt="Metric Minds Logo" />
+      <div>
+        <div className="text-xl font-medium text-black dark:text-white">
+          Metric Minds
+        </div>
+        {/*<p className="text-gray-500 dark:text-gray-400">You have a new message!</p>*/}
+      </div>
       <Button
         variant="outline"
         className="bg-green-500 hover:bg-green-600 text-white"
@@ -13,5 +20,16 @@ export const LandingRoute = () => {
         Login with Spotify
       </Button>
     </div>
+
+    // <div className="flex flex-col items-center justify-center min-h-screen">
+    //   <h1 className="text-3xl font-bold mb-8">Metric Minds</h1>
+    //   <Button
+    //     variant="outline"
+    //     className="bg-green-500 hover:bg-green-600 text-white"
+    //     onClick={async () => (window.location.href = await login())}
+    //   >
+    //     Login with Spotify
+    //   </Button>
+    // </div>
   );
 };
